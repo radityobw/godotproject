3acb6a3a317ff09fa17eeae1297605c5daa3ab58
radityobw <155155675+radityobw@users.noreply.github.com>
2025-11-02 15:11:21 +0700

    .

--- script/level_select.gd
+++ script/level_select.gd
@@ -4,11 +4,13 @@ extends Control
 func lv1_on_pressed() -> void:
 	SelectSfx.play()
 	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://scene/lv1.tscn")
 
 
 func lv2_on_pressed() -> void:
 	SelectSfx.play()
 	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://scene/lv2.tscn")
 
 
 func _on_home_pressed() -> void:
